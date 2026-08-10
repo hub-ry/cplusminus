091862f46e83196517aad4cc84ec2ea00aad6edd
Ryan Hubbart <rhubbart@purdue.edu>
2026-08-10 14:47:52 -0500

    Time: 2 ms (23.79%) | Memory: 8.4 MB (95.93%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     string decodeString(string s) {
-        std::stack<int> numbers;
-        std::stack<string> strings;
-        int currNum = 0;
-        std::string currString = "";
-        for (char x : s) {
-            if (isdigit(x)) {
-                currNum = currNum * 10 + (x - '0');
-            }
-            else if (x == '[') {
-                numbers.push(currNum);
-                strings.push(currString);
-                currNum = 0;
-                currString = "";
-            }
-            else if (x == ']') {
-                //write strings.top() numbers.top() times
-                string repeated = "";
-                for (int i = 0; i < numbers.top(); i++) {
-                    repeated += currString;
-                }
-                string prev = strings.top();
-                strings.pop();
-                currString = prev + repeated;
-                numbers.pop();
+        std::stack<pair<int, string>> x;
+        int num = 0;
+        string temp = "";
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '[') {
+                x.push({num, temp});
+                num = 0;
+                temp = "";
+            } else if (c == ']') {
+                auto [mult, msg] = x.top();
+                x.pop();
 
+                string chunk = temp;
+                temp = msg;
+                for (int i = 0; i < mult; i++) {
+                    temp += chunk;
+                }
+                
+            } else {
+                temp += c;
             }
-            else {
-                currString += x;
-            }
-            
         }
-        return currString;
+        return temp;
     }
 };
