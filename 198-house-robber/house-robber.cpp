eaf96239cc66f1dec210d00c95018375bccfee6f
Ryan Hubbart <rhubbart@purdue.edu>
2026-08-27 16:49:01 -0400

    Time: 0 ms (100.00%) | Memory: 10.8 MB (24.07%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return nums[0];
+        }
+        if (n < 3) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(n);
+        
+        
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[2] + nums[0];
+
+        for (int i = 3; i < n; i++) {
+            dp[i] = nums[i] + max(dp[i - 3], dp[i - 2]);
+        }
+
+        return max(dp[n-1], dp[n-2]);
+    }
+};
